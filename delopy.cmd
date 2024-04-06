53a39081f9124c46cb4b858fb979de3236da5f19
un1fyz <3336016933@qq.com>
2024-04-06 17:14:54 +0800

    '鏇存柊鏂囨。'

--- delopy.cmd
+++ delopy.cmd
@@ -1,4 +1,4 @@
-npm run docs:build && cd D:\UserData\Documents\vuepress\docs && git init && git add -A && git commit -m 'deploy' && git push -f git@github.com:UNi-Network0/OpenBMCLAPI-Dashboard-APIDOC.git  master:main
+npm run docs:build && cd D:\UserData\Documents\vuepress\docs && git init && git add -A && git commit -m '更新文档' && git push -f git@github.com:UNi-Network0/OpenBMCLAPI-Dashboard-APIDOC.git  master:main
 
 
 
